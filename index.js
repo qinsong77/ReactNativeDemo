3c8ea6a6327f197c16855b49de84f00422a548ef
qinsong <qinsong1@cmiot.chinamobile.com>
2019-05-20 16:01:43 +0800

    feat: add react-navigation demo

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
  * @format
  */
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
 // import App from './src/examples/testApp';
 // import App from './src/examples/TodoList/app';
 // import App from './src/examples/SectionList';
+// import App from './src/examples/react_navigation/demo1';
+import App from './src/examples/react_navigation/bottom_tabs_demo';
 import {name as appName} from './app.json';
 
 //下面两行是屏蔽警告
